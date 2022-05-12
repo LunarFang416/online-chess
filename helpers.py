d0ee5234e5134968e2ca1eed79adef4c7ad2a211
Abubukker Chaudhary <abubukker.chaudhary@gmail.com>
2022-05-11 22:29:36 -0400

    changes helpers

--- helpers.py
+++ helpers.py
@@ -1,27 +1,2 @@
-from typing import List, Tuple
-
-
-
-WHITE_PAWN_POS = [[6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7]]
-WHITE_QUEEN_POS = [[7, 3]]
-WHITE_KING_POS = [[7, 4]]
-WHITE_ROOK_POS = [[7, 0], [7, 7]]
-WHITE_BISHOP_POS = [[7, 2], [7, 5]]
-WHITE_KNIGHT_POS = [[7, 1], [7, 6]]
-
-BLACK_PAWN_POS = [[1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]]
-BLACK_QUEEN_POS = [[0, 3]]
-BLACK_KING_POS = [[0, 4]]
-BLACK_ROOK_POS = [[0, 0], [0, 7]]
-BLACK_BISHOP_POS = [[0, 2], [0, 5]]
-BLACK_KNIGHT_POS = [[0, 1], [0, 6]]
-
-
 def legal_move(x_pos: int, y_pos: int, r_dir: int, c_dir, max_row:int, max_col) -> bool:
     return 0 <= x_pos + r_dir < max_row and 0 <= y_pos + c_dir < max_col
-
-
-
-
-
-
