8520d1450c24791ffffd6711a432739ab9f57211
Abubukker Chaudhary <abubukker.chaudhary@gmail.com>
2022-05-12 20:46:45 -0400

    server setup

---
+++ client.py
@@ -0,0 +1,38 @@
+import socket 
+import pickle
+import time
+
+HEADER = 4096
+PORT = 5050
+FORMAT = 'utf-8'
+SERVER = "localhost"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect()
+
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = SERVER
+        self.port = PORT
+        self.addr = (self.server, self.port)
+        self.board = self.connect()
+        self.board = pickle.loads(self.board)
+
+    def connect(self):
+        self.client.connect(self.addr)
+        return self.client.recv(HEADER*8)
+
+    def disconnect(self, data):
+        self.client.close()
+        try:
+            self.client.send(pickle.dumps(data))
+            reply = self.client.recv(HEADER*8)
+        except Exception as e:
+            print(e)
+
+        return reply
--- server.py
+++ server.py
@@ -1,3 +1,36 @@
 import socket
 import threading
 
+HEADER = 4096
+PORT = 5050
+SERVER = "localhost"
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER*8)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE: 
+            connected = False
+        print(f"[{addr}] {msg}")
+    conn.close()
+
+def start():
+    print(f"[STARTING] server is starting on port {PORT}")
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, ARGS=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+start()
